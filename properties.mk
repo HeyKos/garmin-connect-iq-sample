8c4315a930260b432161f6395737e5e0f31d9250
mikekoser <mikekoser@gmail.com>
2021-02-14 15:15:17 -0500

    Removed properties.mk from repo.

--- properties.mk
+++
@@ -1,4 +0,0 @@
-DEVICE = vivoactive3m
-SDK_HOME = "/Users/mikekoser/Library/Application Support/Garmin/ConnectIQ/Sdks/connectiq-sdk"
-DEPLOY = /Volumes/GARMIN/GARMIN/APPS/
-PRIVATE_KEY = developer_key.der
